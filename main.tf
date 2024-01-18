157c592d790538127786d4c06552088454dd4de4
nalinture <68896641+nalinture@users.noreply.github.com>
2024-01-18 22:16:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
            name = "terraform_tutorial"
          }
      }
-}
+
 
  required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.74.2"
+      # version = "3.74.2"
     }
   }
 }
