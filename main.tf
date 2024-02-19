6a3448edb67a9e77789a660638086ce36235f474
nalinture <68896641+nalinture@users.noreply.github.com>
2024-02-19 18:28:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,13 +29,10 @@ resource "aws_iam_role" "lambda_role" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": [
-          "sts:AssumeRole"
-      ],
+      "Action": "sts:AssumeRole",
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
-      "Resource": "*",
       "Effect": "Allow",
       "Sid": ""
     }
