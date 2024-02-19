b90e051896bd15a32311df2164091b3340950739
nalinture <68896641+nalinture@users.noreply.github.com>
2024-02-19 18:06:19 +0530

    Update hello-python.py

--- python/hello-python.py
+++ python/hello-python.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-   message = 'Hello {} !'.format(event['key1'])
+   message = 'Vanakkam {} !'.format(event['key1'])
    return {
        'message' : message
    }
